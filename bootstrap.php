d2c9d697146985e595c6e1b4f65e875777b44ee2
ndkingsley <nate@ndkingsley.com>
2013-10-24 14:50:17 -0400

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER',false);
+defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER',false);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+$_SERVER['SCRIPT_NAME']='/'.basename(__FILE__);
+$_SERVER['SCRIPT_FILENAME']=__FILE__;
+
+class CTestCase extends PHPUnit_Framework_TestCase
+{
+}
