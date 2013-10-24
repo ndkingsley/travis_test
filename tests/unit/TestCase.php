cb6d8dec8c6967f5c0c06d04a7958c7ab3e7d588
ndkingsley <nate@ndkingsley.com>
2013-10-24 15:06:10 -0400

    Update TestCase.php

--- tests/unit/TestCase.php
+++ tests/unit/TestCase.php
@@ -1,3 +1,13 @@
 <?php
+
+  require_once(dirname(__FILE__).'/../bootstrap.php');
   
   
+  class TestCaseTest extends CTestCase {
+
+        protected function setUp()
+        {
+                parent::setUp();
+        }
+    
+  }
